fcafd6fea336c4641d496bfb88c8a570a928702e
Jessie Tse <szewingtse@gmail.com>
2018-10-13 10:30:40 +0800

    Resume
    Resume

--- 2_script.js
+++ 2_script.js
@@ -1,5 +1,5 @@
 
-        function age_cal()
+    function age_cal()
         {
           var dog_age;
           dog_age=Number(document.dcal.txtdog_age.value);
@@ -7,3 +7,12 @@
           document.dcal.txthuman_age.value=res;
 
         }
+
+    function show_topic()
+        {
+            var x = document.getElementById("demo");
+            var my_name = prompt("Enter your your name")
+            x.style.fontSize = "25px";
+            x.style.color = "red";
+            document.getElementById("demo").innerHTML = "My name is " + my_name;
+          }
