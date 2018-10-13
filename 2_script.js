6f399e0cd52e56de7abbb238db9924fb587778af
Jessie Tse <szewingtse@gmail.com>
2018-10-13 10:26:35 +0800

    PS2
    Put script in external

---
+++ 2_script.js
@@ -0,0 +1,9 @@
+
+        function age_cal()
+        {
+          var dog_age;
+          dog_age=Number(document.dcal.txtdog_age.value);
+          res=dog_age*7;
+          document.dcal.txthuman_age.value=res;
+
+        }
