574964fd7e0630f53b8d7915b86e82412181e8a1
Jessie Tse <szewingtse@gmail.com>
2018-10-13 10:56:34 +0800

    Update in lesson
    change file location

--- 2_script.js
+++ 2_script.js
@@ -1,6 +1,7 @@
 
     function age_cal()
         {
+          alert("Check out how old is your dog!")
           var dog_age;
           dog_age=Number(document.dcal.txtdog_age.value);
           res=dog_age*7;
@@ -11,8 +12,8 @@
     function show_topic()
         {
             var x = document.getElementById("demo");
-            var my_name = prompt("Enter your your name")
+            var dog_name = prompt("Enter your dog's name")
             x.style.fontSize = "25px";
-            x.style.color = "red";
-            document.getElementById("demo").innerHTML = "My name is " + my_name;
+            x.style.color = "#B3DA8C";
+            document.getElementById("demo").innerHTML = "Woof! My name is " + dog_name;
           }
